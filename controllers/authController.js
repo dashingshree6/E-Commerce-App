aa1169707482b635f42adf327199f2b22dabe138
dashingshree6 <dashingshree6@gmail.com>
2022-05-12 19:46:56 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,3 +43,45 @@ module.exports.signup = (req, res) => {
         })
     })
 }
+
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body
+
+    if(!email || !password) {
+        res.status(400).json({msg: "Please enter all the fields"})
+    }
+
+    User.findOne({email})
+    .then(user => {
+        if(!user) return res.status(400).json({msg: "User doesnot exists"})
+    })
+
+    bcrypt.compare(password, user.password)
+    .then(isMatch => {
+        if(!isMatch) return res.status(400).json({msg: "Invalid credentials"})
+    })
+
+
+                jwt.sign(
+                    {id: user_id},
+                    config.get('jwtsecret'),
+                    {expiresIn: 3600},
+                    (err, token) => {
+                        if(err) throw err;
+                        res.json({
+                            token,
+                            user: {
+                                id: user._id,
+                                name: user.name,
+                                email: user.name
+                            }
+                        })
+                    }
+                )
+}
+
+module.exports.get_user = (req, res) => {
+    Usser.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user))
+}
