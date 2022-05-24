f923e05d3761ca7df8141e03f3fd5738408ff684
dashingshree6 <dashingshree6@gmail.com>
2022-05-24 18:19:52 +0530

    Create orderController.js

---
+++ controllers/orderController.js
@@ -0,0 +1,10 @@
+const Order = require('../models/Order')
+const Cart = require('../models/Cart')
+const User = require('../models/User')
+const config = require('config')
+const stripe = require('stripe')(config.get('StripeAPIKey'))
+
+module.exports.get_orders = async (req,res) => {
+    const userId = req.params.id
+    Order.find({userId}).sort({date:-1}).then(orders => res.json(orders))
+}
