19995a5e6ba0338391a4f58b33e1c80430f7a633
dashingshree6 <dashingshree6@gmail.com>
2022-05-24 18:33:21 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,3 +8,40 @@ module.exports.get_orders = async (req,res) => {
     const userId = req.params.id
     Order.find({userId}).sort({date:-1}).then(orders => res.json(orders))
 }
+
+module.exports.checkout = async (req, res) => {
+    try {
+        const userId = req.params.id
+        const {source} = req.body
+        let cart = await Cart.findOne({userId})
+        let user = await User.findOne({_id:userId})
+        const email = user.email
+
+        if (cart) {
+            const charge = await stripe.charges.create({
+                amount: cart.bill,
+                currency: 'inr',
+                source: source,
+                receipt_email: email
+            })
+            if (!charge) {
+                throw error('Payment Failed')
+            }
+            if (charge) {
+                const order = await Order.create({
+                    userId,
+                    items: cart.items,
+                    bill: cart.bill
+                })
+                const data = await Cart.findByIdAndDelete({_id:cart.id})
+                return res.status(201).send(order)
+            }
+        } else {
+            return res.status(500).send('You donot hve items in cart')
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Something went wrong')
+    }
+}
